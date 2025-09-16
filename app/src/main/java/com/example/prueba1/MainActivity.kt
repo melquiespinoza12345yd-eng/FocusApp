f9ea6e218be698a82c80afe9646c603f41bc338c
Melquisedec <Melquiespinoza12345yd@gmail.com>
2025-09-16 20:46:11 -0300

    init: proyecto base

---
+++ app/src/main/java/com/example/prueba1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prueba1 // ¡MUY IMPORTANTE: CAMBIA ESTO AL NOMBRE DE TU PAQUETE SI ES DIFERENTE!
+
+package com.yellowpath.core
+adb devices
+
---
+++ app/src/main/java/com/example/prueba1/PomodoroViewModel.kt
@@ -0,0 +1,3 @@
+package com.example.prueba1
+
+annotation class PomodoroViewModel
